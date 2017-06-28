ec7c0aaf9eba067b8bdecc28e34558a1cc192993
Pedro Abel <Usuario@GGMCPLABWRK001.espol.edu.ec>
2017-06-28 09:48:59 -1000

    init

---
+++ AuthServer/AuthServer/Startup.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Reflection;
+
+namespace AuthServer
+{
+    public class Startup
+    {
+        public Startup(ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(LogLevel.Debug);
+        }
+
+        //public IConfigurationRoot Configuration { get; }
+
+        //public Startup(IHostingEnvironment env)
+        //{
+        //    var builder = new ConfigurationBuilder()
+        //            .SetBasePath(env.ContentRootPath)
+        //            .AddJsonFile("appsettings.json", true, true)
+        //            .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+        //    builder.AddEnvironmentVariables();
+        //    Configuration = builder.Build();
+        //}
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // configure identity server with in-memory stores, keys, clients and scopes
+            services.AddIdentityServer()
+                .AddTemporarySigningCredential()
+                .AddInMemoryApiResources(Config.GetApiResources())
+                .AddInMemoryClients(Config.GetClients())
+                .AddTestUsers(Config.GetUsers());
+
+            //var connectionString = Configuration.GetConnectionString("DefaultConnection");
+            //var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+            //services.AddDbContext<ApplicationDbContext>(options =>
+            //        options.UseSqlServer(connectionString)
+            //);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseIdentityServer();
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
